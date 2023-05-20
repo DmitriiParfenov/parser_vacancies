5379a815a4e49f5dd4f9d037cad0232c3d21f331
Dmitrii <dima12sik@yandex.ru>
2023-05-20 23:10:43 +0300

    add test_add_data_to_empty_file to check header in the empty file

--- tests/test_db_operation.py
+++ tests/test_db_operation.py
@@ -1,3 +1,5 @@
+import csv
+
 import pytest
 
 from utils.db_operation import CSVSaver, Adder
@@ -11,3 +13,12 @@ def test_subclasses_csv_saver():
 def test_add_incorrect_data_to_file(get_instance_saver, expected, argument):
     with pytest.raises(expected):
         get_instance_saver.add_vacancy(argument)
+
+
+def test_add_data_to_empty_file(get_empty_file, get_instance_saver, vacancies_examples):
+    get_instance_saver.add_vacancy(vacancies_examples[0])
+    with open('db_vacancies.csv', 'r', newline='', encoding='UTF-16') as file:
+        reader = csv.reader(file, delimiter='\t')
+        data = [row for row in reader]
+        assert data[0] == ['Идентификатор', 'Название_вакансии', 'Ссылка', 'Зарплата_от', 'Зарплата_до',
+                           'Имя_нанимателя', 'Город', 'Описание_вакансии', 'Требование', 'Опыт', 'Дата_публикации']
