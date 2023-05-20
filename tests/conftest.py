dee45450b7c62d46e095d1cd497f7cd70b33002f
Dmitrii <dima12sik@yandex.ru>
2023-05-20 23:17:40 +0300

    updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime as dt
 
 import pytest
@@ -25,3 +26,24 @@ def vacancies_examples():
 @pytest.fixture
 def get_instance_saver():
     return CSVSaver()
+
+
+@pytest.fixture
+def get_empty_file():
+    with open('db_vacancies.csv', 'w', newline='', encoding='UTF-16'):
+        pass
+
+
+@pytest.fixture
+def get_file_with_two_rows(vacancies_examples):
+    with open('db_vacancies.csv', 'w', newline='', encoding='UTF-16') as file:
+        writer = csv.writer(file, delimiter='\t')
+        head_row = ('Идентификатор', 'Название_вакансии', 'Ссылка', 'Зарплата_от', 'Зарплата_до',
+                    'Имя_нанимателя', 'Город', 'Описание_вакансии', 'Требование', 'Опыт', 'Дата_публикации')
+        writer.writerow(head_row)
+        data_row = (vacancies_examples[0].id, vacancies_examples[0].title, vacancies_examples[0].url,
+                    vacancies_examples[0].salary_from,
+                    vacancies_examples[0].salary_to, vacancies_examples[0].name_employer,
+                    vacancies_examples[0].city, vacancies_examples[0].description,
+                    vacancies_examples[0].requirement, vacancies_examples[0].experience, vacancies_examples[0].date)
+        writer.writerow(data_row)
