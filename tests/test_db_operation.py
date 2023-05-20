5794e04549e947e06d8cc3cd10ef5504a30c9dcf
Dmitrii <dima12sik@yandex.ru>
2023-05-20 22:34:47 +0300

    add test_add_incorrect_data_to_file to add correct data

--- tests/test_db_operation.py
+++ tests/test_db_operation.py
@@ -1,5 +1,3 @@
-from datetime import datetime as dt
-
 import pytest
 
 from utils.db_operation import CSVSaver, Adder
@@ -7,3 +5,9 @@ from utils.db_operation import CSVSaver, Adder
 
 def test_subclasses_csv_saver():
     assert issubclass(CSVSaver, Adder) is True
+
+
+@pytest.mark.parametrize("expected, argument", [(TypeError, '1'), (TypeError, 250), (TypeError, {'a': 1})])
+def test_add_incorrect_data_to_file(get_instance_saver, expected, argument):
+    with pytest.raises(expected):
+        get_instance_saver.add_vacancy(argument)
