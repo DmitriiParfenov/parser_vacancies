0c4ddd344bd1a1778dadfe3626d7f55cea3546fb
Dmitrii <dima12sik@yandex.ru>
2023-05-21 14:48:36 +0300

    add docstring to tests

--- tests/test_class_operations.py
+++ tests/test_class_operations.py
@@ -31,11 +31,13 @@ from utils.class_operations import Vacancy
                           (TypeError, 1, 'title', 'https://ex.ru', 3, 5, 'name', 'city', 'desc', 'req', 1,
                            '2020-01-01')])
 def test_vacancy_validate_data_1(id_, title, url, salary_from, salary_to, name, city, desc, req, exp, date, expected):
+    """Метод валидирует входные данные при инициализации экземпляров класса Vacancy."""
     with pytest.raises(expected):
         Vacancy(id_, title, url, salary_from, salary_to, name, city, desc, req, exp, date)
 
 
 def test_vacancy_validate_data_2(vacancies_examples):
+    """Каждый элемент в фикстуре должен быть экземпляром класса Vacancy."""
     result = []
     for elem in vacancies_examples:
         result.append(isinstance(elem, Vacancy))
@@ -43,6 +45,7 @@ def test_vacancy_validate_data_2(vacancies_examples):
 
 
 def test_vacancy_get_id(vacancies_examples):
+    """Метод проверяет наличие id у каждого экземпляра класса Vacancy."""
     result = []
     for elem in vacancies_examples:
         result.append(elem.id)
@@ -50,6 +53,7 @@ def test_vacancy_get_id(vacancies_examples):
 
 
 def test_vacancy_get_url(vacancies_examples):
+    """Метод проверяет наличие url у каждого экземпляра класса Vacancy."""
     result = []
     for elem in vacancies_examples:
         result.append(elem.url)
@@ -70,12 +74,20 @@ def test_vacancy_get_url(vacancies_examples):
                                                                 (10, 4, 'date — 2023-01-05 00:00:00'),
                                                                 ])
 def test_vacancy_method__str__(vacancies_examples, inner_index, outer_index, expected):
+    """
+    При вызове метода str() должна возвращаться строка с атрибутами экземпляра класса Vacancy в дружественном
+    формате.
+    """
     test_object = vacancies_examples[outer_index]
     result = str(test_object).split('\n')
     assert result[inner_index] == expected
 
 
 def test_vacancy_method__repr__(vacancies_examples):
+    """
+    При вызове метода repr() должна возвращаться строка с названием класса и аргументами, которые должны передваться
+    при инициализации экземпляров этого класса.
+    """
     assert repr(vacancies_examples[0]) == (
         "Vacancy(1, 'Биоинформатик', 'https://example1.ru/', 30000, 60000, 'BIOCAD', "
         "'Санкт-Петербург', 'Описание_1', 'Секвенирование','Нет опыта работы', "
@@ -85,6 +97,10 @@ def test_vacancy_method__repr__(vacancies_examples):
 @pytest.mark.parametrize("expected, inner_index, argument", [(ValueError, 0, 10), (ValueError, 1, 15),
                                                              (ValueError, 4, 20), (ValueError, 2, -100)])
 def test_vacancy_method__eq__1(vacancies_examples, expected, inner_index, argument):
+    """
+    Метод __eq__ должен корректно работать только между экземплярами одного или унаследованного классов,
+    в ином случае возбудится исключение.
+    """
     with pytest.raises(expected):
         result = vacancies_examples[inner_index] == argument
 
@@ -92,12 +108,19 @@ def test_vacancy_method__eq__1(vacancies_examples, expected, inner_index, argume
 @pytest.mark.parametrize("index_1, index_2, expected", [(0, 1, False), (4, 3, True), (2, 3, False),
                                                         (3, 4, True), (2, 2, True)])
 def test_vacancy_method__eq__2(vacancies_examples, index_1, index_2, expected):
+    """
+    Метод __eq__ должен корректно работать только между экземплярами одного класса или между унаследованных классов.
+    """
     assert (vacancies_examples[index_1] == vacancies_examples[index_2]) == expected
 
 
 @pytest.mark.parametrize("expected, inner_index, argument", [(ValueError, 0, 10), (ValueError, 1, 15),
                                                              (ValueError, 4, 20), (ValueError, 2, -100)])
 def test_vacancy_method__compare__1(vacancies_examples, expected, inner_index, argument):
+    """
+    Метод сравнения (больше) по зарплате должен корректно работать только между экземплярами одного
+    или унаследованного классов, в ином случае возбудится исключение.
+    """
     with pytest.raises(expected):
         result = vacancies_examples[inner_index] > argument
 
@@ -105,6 +128,10 @@ def test_vacancy_method__compare__1(vacancies_examples, expected, inner_index, a
 @pytest.mark.parametrize("expected, inner_index, argument", [(ValueError, 2, 10), (ValueError, 3, 15),
                                                              (ValueError, 4, 20), (ValueError, 1, -100)])
 def test_vacancy_method__compare__2(vacancies_examples, expected, inner_index, argument):
+    """
+    Метод сравнения (больше или равно) по зарплате должен корректно работать только между экземплярами
+    одного или унаследованного классов, в ином случае возбудится исключение.
+    """
     with pytest.raises(expected):
         result = vacancies_examples[inner_index] >= argument
 
@@ -112,22 +139,38 @@ def test_vacancy_method__compare__2(vacancies_examples, expected, inner_index, a
 @pytest.mark.parametrize("index_1, index_2, expected", [(0, 1, False), (0, 2, True), (2, 3, False),
                                                         (3, 2, True), (1, 1, False), (1, 4, False)])
 def test_vacancy_method__compare__3(vacancies_examples, index_1, index_2, expected):
+    """
+    Метод сравнения (больше) по зарплате должен корректно работать только между экземплярами одного класса
+    или между унаследованных классов.
+    """
     assert (vacancies_examples[index_1] > vacancies_examples[index_2]) == expected
 
 
 @pytest.mark.parametrize("index_1, index_2, expected", [(0, 1, False), (0, 2, True), (2, 3, False),
                                                         (3, 2, True), (1, 1, True), (1, 4, False)])
 def test_vacancy_method__compare__4(vacancies_examples, index_1, index_2, expected):
+    """
+    Метод сравнения (больше или равно) по зарплате должен корректно работать только между экземплярами
+    одного класса или между унаследованных классов.
+    """
     assert (vacancies_examples[index_1] >= vacancies_examples[index_2]) == expected
 
 
 @pytest.mark.parametrize("index_1, index_2, expected", [(1, 0, False), (4, 0, False), (3, 1, False),
                                                         (2, 0, True), (1, 1, False), (2, 4, True)])
 def test_vacancy_method__compare__5(vacancies_examples, index_1, index_2, expected):
+    """
+    Метод сравнения (меньше) по зарплате должен корректно работать только между экземплярами одного
+    или унаследованного классов.
+    """
     assert (vacancies_examples[index_1] < vacancies_examples[index_2]) == expected
 
 
 @pytest.mark.parametrize("index_1, index_2, expected", [(1, 0, False), (4, 0, False), (3, 1, False),
                                                         (2, 0, True), (1, 1, True), (2, 4, True)])
 def test_vacancy_method__compare__6(vacancies_examples, index_1, index_2, expected):
+    """
+    Метод сравнения (меньше или равно) по зарплате должен корректно работать только между экземплярами одного
+    или унаследованного классов.
+    """
     assert (vacancies_examples[index_1] <= vacancies_examples[index_2]) == expected
