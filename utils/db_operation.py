d4de4dea9c48a2ec411e993124e63c86186fa969
Dmitrii <dima12sik@yandex.ru>
2023-05-21 15:01:55 +0300

    add CSVSaver class into db_operation.py with add_vacancy method in accordance with tests

--- utils/db_operation.py
+++ utils/db_operation.py
@@ -14,3 +14,23 @@ class Adder(ABC):
     def add_vacancy(vacancy):
         """Абстрактный метод, который должен быть переопределен в дочерных классах родительского класса Adder."""
         pass
+
+
+class CSVSaver(Adder):
+    """Класс для работы с данными в файле в формате csv."""
+
+    @staticmethod
+    def add_vacancy(vacancy):
+        """
+        Метод добавляет в файл в формате csv вакансию, информация об которой указана в vacancy. Передававаемый
+        аргумент должен быть экземпляром класса Vacancy, иначе возникнет исключение TypeError.
+        """
+
+        if not isinstance(vacancy, Vacancy):
+            raise TypeError('Передаваемый аргумент должен быть экземпляром класса Vacancy.')
+        with open('db_vacancies.csv', 'a', newline='', encoding='UTF-16') as file:
+            if os.stat('db_vacancies.csv').st_size == 0:
+                writer = csv.writer(file, delimiter='\t')
+                head_row = ('Идентификатор', 'Название_вакансии', 'Ссылка', 'Зарплата_от', 'Зарплата_до',
+                            'Имя_нанимателя', 'Город', 'Описание_вакансии', 'Требование', 'Опыт', 'Дата_публикации')
+                writer.writerow(head_row)
