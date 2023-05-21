c530b933f0bef9c3509427a60e22bebc6d98e466
Dmitrii <dima12sik@yandex.ru>
2023-05-21 15:02:37 +0300

    updated add_vacancy() in CSVSaver

--- utils/db_operation.py
+++ utils/db_operation.py
@@ -34,3 +34,15 @@ class CSVSaver(Adder):
                 head_row = ('Идентификатор', 'Название_вакансии', 'Ссылка', 'Зарплата_от', 'Зарплата_до',
                             'Имя_нанимателя', 'Город', 'Описание_вакансии', 'Требование', 'Опыт', 'Дата_публикации')
                 writer.writerow(head_row)
+
+        with open('db_vacancies.csv', 'r', newline='', encoding='UTF-16') as read_to_file:
+            reader = csv.DictReader(read_to_file, delimiter='\t')
+            data = [x.get('Ссылка') for x in reader]
+            if vacancy.url not in data:
+                with open('db_vacancies.csv', 'a', newline='', encoding='UTF-16') as add_to_file:
+                    writer = csv.writer(add_to_file, delimiter='\t')
+                    info_from_vacancy = (vacancy.id, vacancy.title, vacancy.url, vacancy.salary_from,
+                                         vacancy.salary_to, vacancy.name_employer,
+                                         vacancy.city, vacancy.description,
+                                         vacancy.requirement, vacancy.experience, vacancy.date)
+                    writer.writerow(info_from_vacancy)
